e7741c96073b33264d04152b0a3414c159a689a8
Manuel Lera Ramirez <manulera14@gmail.com>
2021-04-23 11:33:47 +0200

    second attempt

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="preconfig-nedelec", # Replace with your own username
+    name="preconfig", # Replace with your own username
     version="1.0",
     author="Francois Nedelec",
     author_email="francois.nedelec@slcu.cam.ac.uk",
